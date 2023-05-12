834b26a53331ee3cc8ffd11d96a69688f39af76c
K. Travis Walsh <github@traviswalsh.com>
2023-05-12 16:52:02 -0400

    rename test vars

--- employeeservice/src/test/java/com/traviswalsh/employee/EmployeeApplicationTests.java
+++ employeeservice/src/test/java/com/traviswalsh/employee/EmployeeApplicationTests.java
@@ -1,6 +1,5 @@
 package com.traviswalsh.employee;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,87 +15,85 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class EmployeeApplicationTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-	private int testHourlyEmployeeId = 1;
-	//in exhaustive testing all 3 types would be tested for valid values
-    private int testSalariedEmployeeId = 11;
-	private int testManagerEmployeeId = 21;
+        private int testHourlyEmployeeId = 1;
+        private int testSalariedEmployeeId = 11;
+        private int testManagerEmployeeId = 21;
+        private int testLifecycleEmployeeId = 2;
+        @Test
+        // Tests the requirements rather than functionality
+        public void testEmployees() throws Exception {
+                // are 30 employees created on startup
+                mockMvc.perform(get("/employees").accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.length()").value(30));
+                // is a 404 returned when an employee id is not found
+                mockMvc.perform(get("/{id}/employee", 99).accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().is4xxClientError());
 
-	@Test
-	//Tests the requirements rather than functionality
-	public void testEmployees() throws Exception {
-		//are 30 employees created on startup
-        mockMvc.perform(get("/employees").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-				.andExpect(jsonPath("$.length()").value(30));
-		//is a 404 returned when an employee id is not found 
-		mockMvc.perform(get("/{id}/employee", 99).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().is4xxClientError());
-		
+        }
 
-    }	
+        @Test
+        public void testWork() throws Exception {
+                // add 5 days worked
+                mockMvc.perform(post("/{id}/work", testLifecycleEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("5"))
+                                .andExpect(status().isOk());
+                // add more than the number of days available (expect exception 409 from
+                // service)
+                mockMvc.perform(post("/{id}/work", testLifecycleEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("261"))
+                                .andExpect(status().is4xxClientError());
 
-    @Test
-    public void testWork() throws Exception {
-		//add 5 days worked
-        mockMvc.perform(post("/{id}/work", testHourlyEmployeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("5"))
-                .andExpect(status().isOk());
-		//add more than the number of days available (expect exception 409 from service)
-		mockMvc.perform(post("/{id}/work", testHourlyEmployeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("261"))
-                .andExpect(status().is4xxClientError());
-		
-		//test values of vacation accrual
-        //hourly
-		mockMvc.perform(post("/{id}/work", 2)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("260"))
-                .andExpect(status().isOk());
-		mockMvc.perform(get("/{id}/employee", 2).accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.vacationDays").value(10));
-		//salaried
-        mockMvc.perform(post("/{id}/work", testSalariedEmployeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("260"))
-                .andExpect(status().isOk());
-		mockMvc.perform(get("/{id}/employee", testSalariedEmployeeId).accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.vacationDays").value(15));
-		//manager
-		mockMvc.perform(post("/{id}/work", testManagerEmployeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("260"))
-                .andExpect(status().isOk());
-		mockMvc.perform(get("/{id}/employee", testManagerEmployeeId).accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.vacationDays").value(30));
-    }
+                // test values of vacation accrual
+                // hourly
+                mockMvc.perform(post("/{id}/work", testHourlyEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("260"))
+                                .andExpect(status().isOk());
+                mockMvc.perform(get("/{id}/employee", testHourlyEmployeeId).accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.vacationDays").value(10));
+                // salaried
+                mockMvc.perform(post("/{id}/work", testSalariedEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("260"))
+                                .andExpect(status().isOk());
+                mockMvc.perform(get("/{id}/employee", testSalariedEmployeeId).accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.vacationDays").value(15));
+                // manager
+                mockMvc.perform(post("/{id}/work", testManagerEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("260"))
+                                .andExpect(status().isOk());
+                mockMvc.perform(get("/{id}/employee", testManagerEmployeeId).accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.vacationDays").value(30));
+        }
 
-    @Test
-    public void testTakeVacation() throws Exception {
+        @Test
+        public void testTakeVacation() throws Exception {
 
-		//first add vacation for the employee
-		mockMvc.perform(post("/{id}/work", testHourlyEmployeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("100"))
-                .andExpect(status().isOk());
-		//now take vacation
-        mockMvc.perform(post("/{id}/take-vacation", testHourlyEmployeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("2.3"))
-                .andExpect(status().isOk());
-		//take more vacation than is allowed
-        mockMvc.perform(post("/{id}/take-vacation", testHourlyEmployeeId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("100"))
-                .andExpect(status().is4xxClientError());
+                // first add vacation for the employee
+                mockMvc.perform(post("/{id}/work", testLifecycleEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("100"))
+                                .andExpect(status().isOk());
+                // now take vacation
+                mockMvc.perform(post("/{id}/take-vacation", testLifecycleEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("2.3"))
+                                .andExpect(status().isOk());
+                // take more vacation than is allowed
+                mockMvc.perform(post("/{id}/take-vacation", testLifecycleEmployeeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("100"))
+                                .andExpect(status().is4xxClientError());
 
-    }
+        }
 }
-
